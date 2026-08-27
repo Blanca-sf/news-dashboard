ec998e7b1d5c60dc30b5a60ba47863abbd0d9b94
Blanca Liliana Sanchez Flores <bsanchezflores00@mylangara.ca>
2026-08-27 16:03:02 -0700

    Fix proxy export to satisfy Next.js function detection

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 
-export const { auth: middleware } = NextAuth(authConfig);
+const { auth } = NextAuth(authConfig);
+
+export function proxy(...args: Parameters<typeof auth>) {
+  return auth(...args);
+}
 
 export const config = {
   matcher: ["/favoritos/:path*"],
